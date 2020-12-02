ca630fb1d94057dd29fbc7076406616e851cae23
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2020-12-02 21:27:37 +0400

    add mobile

--- Javascript/helper.js
+++ Javascript/helper.js
@@ -29,3 +29,31 @@ function setAttributes(el, attrs) {
         el.setAttribute(key, attrs[key]);
     }
 }
+
+window.addEventListener('resize', reportWindowSize);
+// window.addEventListener('onload', reportWindowSize);
+window.document.onload = reportWindowSize();
+
+function reportWindowSize() {
+  let windowWidth = document.getElementById('app').offsetWidth;
+  let windowHeight = document.getElementById('app').offsetHeight;
+  let o_left = null;
+
+  if(windowHeight < windowWidth)
+    windowWidth = windowHeight
+
+  let gameWidth = 740;
+  let gameScale = 0;
+
+  if(windowWidth < 705) {
+    gameScale =  windowWidth / gameWidth;
+
+    document.getElementById('game').style.transform = `scale(${gameScale})`;
+    document.getElementById('game-div').style.left = `${(windowWidth / 2) / gameScale}px`;
+    document.getElementById('game-div').style.marginLeft = `-346px`;
+  } else {
+    document.getElementById('game').style.transform = `scale(1)`;
+    document.getElementById('game-div').style.left = `auto`;
+    document.getElementById('game-div').style.margin = `auto`;
+  }
+}
